b535aa577bf6a15e6cb76f386bb716a005e5214f
dkhobragade <dikshantkhobragade12@gmail.com>
2024-08-17 22:30:47 +0530

    1 questions

---
+++ Strings/Non_Repeating_Character.py
@@ -0,0 +1,10 @@
+# Non Repeating Character
+
+# s = "zxvczbtxyzvy"
+# s = "yy"
+s = "hello"
+for i in range(len(s)):
+    print(s[i : len(s)])
+    # if s[i] not in s[i + 1 : len(s)]:
+    #     print(s[i])
+    #     # break
