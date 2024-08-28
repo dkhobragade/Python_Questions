86d3d0658552abd70dd7c723db21404dcfec3ef9
dkhobragade <dikshantkhobragade12@gmail.com>
2024-08-28 16:28:44 +0530

    Implement_Atoi

--- Strings/Implement_Atoi.py
+++ Strings/Implement_Atoi.py
@@ -2,9 +2,9 @@ s = "123"
 
 print(int("098"))
 
-for i in s:
-    if i.isdigit():
+for i in range(len(s)):
+    if (i == 0 and s[i] == "-") or s[i].isdigit():
         continue
     else:
-        print("ok")
-        break
+        print(-1)
+print(int(s))
