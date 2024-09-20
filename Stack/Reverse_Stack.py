0aa14d5bc4af3403f95049f1dd41bf70b77f88de
dkhobragade <dikshantkhobragade12@gmail.com>
2024-09-20 16:35:15 +0530

    Reverse a Stack

---
+++ Stack/Reverse_Stack.py
@@ -0,0 +1,11 @@
+# Reverse a Stack
+
+St = {3, 2, 1, 7, 6}
+
+print(list(St))
+
+stack_list = list(St)
+reversed_stack = stack_list[::-1]
+
+# Output the reversed stack
+print("Reversed stack:", reversed_stack)
