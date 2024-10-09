df4e7d349dac352f7561ea230a1412b83713322e
dkhobragade <dikshantkhobragade12@gmail.com>
2024-10-09 23:12:12 +0530

    Array Subset

--- Arrays/Array_Subset.py
+++ Arrays/Array_Subset.py
@@ -5,8 +5,8 @@ a1 = [11, 7, 1, 13, 21, 3, 7, 3]
 a2 = [11, 3, 7, 1, 7]
 
 
-for i in a2:
-    if i not in a1:
-        break
-    else:
-        continue
+# for i in a2:
+#     if i not in a1:
+#         break
+#     else:
+#         continue
