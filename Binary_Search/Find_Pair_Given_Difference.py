4769fa9328fc6903807db55b2ab6758454af02fb
dkhobragade <dikshantkhobragade12@gmail.com>
2024-10-11 23:18:47 +0530

    Find Pair Given Difference

---
+++ Binary_Search/Find_Pair_Given_Difference.py
@@ -0,0 +1,13 @@
+# Find Pair Given Difference
+
+
+def findPair(n, x, arr):
+    n = n
+
+
+if __name__ == "__main__":
+    n = 6
+    x = 78
+    arr = [5, 20, 3, 2, 5, 80]
+
+    print(findPair(n, x, arr))
